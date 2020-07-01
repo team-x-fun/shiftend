f2b70e73844b26a37cf80012521b8e76bb0077fb
kouki <kouworld0123@gmail.com>
2020-07-01 21:17:10 +0900

    feat: user_repository_interfaces and mock

---
+++ lib/repositories/interfaces/user_repository_interface.dart
@@ -0,0 +1,7 @@
+import 'package:shiftend/models/models.dart';
+
+abstract class UserRepositoryInterface {
+  Future<void> create(User user);
+  Future<void> update(User user);
+  Future<User> getCurrentUser();
+}
