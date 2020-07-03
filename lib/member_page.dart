e99f4d05c481291f2495a9c768845ad1339f8d22
mattu_uq <kisarailgun@gmail.com>
2020-07-04 00:18:17 +0900

    replace user data to user_repository_mock(doing)

--- lib/member_page.dart
+++ lib/member_page.dart
@@ -1,37 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:shiftend/member.dart';
 import 'package:shiftend/member_item.dart';
+import 'package:shiftend/repositories/interfaces/user_repository_interface.dart';
+import 'package:shiftend/repositories/mocks/user_repository_mock.dart';
+import 'package:shiftend/models/models.dart';
 
 class MemberPage extends StatelessWidget {
+  final UserRepositoryInterface userRepository = UserRepositoryMock();
   @override
-  Widget build(BuildContext context) {
-    const _mockData = [
-      Member(
-          iconUrl:
-              'https://tblg.k-img.com/restaurant/images/Rvw/29971/640x640_rect_29971374.jpg',
-          name: '佐藤勇一郎',
-          role: 'オーナー',
-          level: '100'),
-      Member(
-          iconUrl:
-              'https://tblg.k-img.com/restaurant/images/Rvw/29971/640x640_rect_29971374.jpg',
-          name: '工藤大輔',
-          role: 'オナー',
-          level: '100'),
-      Member(
-          iconUrl:
-              'https://tblg.k-img.com/restaurant/images/Rvw/29971/640x640_rect_29971374.jpg',
-          name: '松山航',
-          role: 'バイトリーダ',
-          level: '100'),
-    ];
-
+  Widget build(BuildContext context) async {
+    List<User> _users = await userRepository.getUsers(); 
+    // asyncとawatは別関数で管理する必要がある？
     return Scaffold(
       body: ListView.builder(
-        itemCount: _mockData.length,
+        //itemCount: _mockData.length,
         itemBuilder: (context, int index) {
-          return MemberItem(member: _mockData[index]);
+          return MemberItem(member: _users[index]);
         },
       ),
     );
