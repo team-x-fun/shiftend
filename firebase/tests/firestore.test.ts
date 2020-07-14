646a53c220d76e7e2dc118ae8524c27adfa046d2
kouki <kouworld0123@gmail.com>
2020-07-14 17:22:51 +0900

    feat: セキュリティールールforUsers

--- firebase/tests/firestore.test.ts
+++ firebase/tests/firestore.test.ts
@@ -16,7 +16,7 @@ const createAdminApp = (): firebase.firestore.Firestore => {
 };
 
 // user情報への参照を作る
-const usersRef = (db: firebase.firestore.Firestore) => db.collection('user');
+const usersRef = (db: firebase.firestore.Firestore) => db.collection('users');
 
 describe('Firestoreセキュリティルール', () => {
   // ルールファイルの読み込み
@@ -35,10 +35,90 @@ describe('Firestoreセキュリティルール', () => {
     await Promise.all(firebase.apps().map(app => app.delete()));
   });
 
-  test('読み書きが可能', async () => {
-    const db = createAuthApp({uid: 'test'});
-    const user = usersRef(db).doc('test');
-    await firebase.assertSucceeds(user.set({name: '太郎'}));
-    await firebase.assertSucceeds(user.get());
+  const collectUser = {
+    id: 'test',
+    email: 'test@example.com',
+    icon_url: 'https://example.com/example.png',
+    name: 'テスト太郎',
+    role: 'アルバイト',
+    level: '100'
+  };
+
+  describe('ユーザ認証情報の検証', () => {
+    test(
+        '自分のuidと同様のドキュメントIDのユーザ情報だけを閲覧，作成，編集，削除可能',
+        async () => {
+          const db = createAuthApp({uid: collectUser.id});
+          const userDocumentRef = usersRef(db).doc(collectUser.id);
+
+          await firebase.assertSucceeds(userDocumentRef.set(collectUser));
+          await firebase.assertSucceeds(userDocumentRef.get());
+
+          await firebase.assertSucceeds(
+              userDocumentRef.update({name: '更新し太郎'}));
+
+          await firebase.assertSucceeds(userDocumentRef.delete());
+        });
+    test(
+        '自分のuidと異なるドキュメントは閲覧，作成，編集，削除ができない',
+        async () => {
+          usersRef(createAdminApp()).doc(collectUser.id).set(collectUser);
+          const db = createAuthApp({uid: 'invalid_user'});
+
+          const userDocumentRef = usersRef(db).doc(collectUser.id);
+
+          await firebase.assertFails(userDocumentRef.set(collectUser));
+
+          await firebase.assertFails(userDocumentRef.get());
+
+          await firebase.assertFails(
+              userDocumentRef.update({name: '更新し太郎'}));
+
+          await firebase.assertFails(userDocumentRef.delete());
+        })
+  });
+
+  describe('user スキーマの検証', () => {
+    test('正しくないスキーマの場合は作成できない', async () => {
+      const db = createAuthApp({uid: collectUser.id});
+      const userDocumentRef = usersRef(db).doc(collectUser.id);
+
+      await firebase.assertFails(
+          userDocumentRef.set({...collectUser, place: 'japan'}));
+
+      await firebase.assertFails(
+          userDocumentRef.set({...collectUser, id: 1234}));
+      await firebase.assertFails(
+          userDocumentRef.set({...collectUser, name: 1234}));
+      await firebase.assertFails(
+          userDocumentRef.set({...collectUser, email: 1234}));
+      await firebase.assertFails(
+          userDocumentRef.set({...collectUser, icon_url: 1234}));
+      await firebase.assertFails(
+          userDocumentRef.set({...collectUser, role: 1234}));
+      await firebase.assertFails(
+          userDocumentRef.set({...collectUser, level: 1234}));
+    });
+    test('正しくないスキーマの場合は編集できない', async () => {
+      usersRef(createAdminApp()).doc(collectUser.id).set(collectUser);
+      const db = createAuthApp({uid: collectUser.id});
+      const userDocumentRef = usersRef(db).doc(collectUser.id);
+
+      await firebase.assertFails(
+          userDocumentRef.update({...collectUser, place: 'japan'}));
+
+      await firebase.assertFails(
+          userDocumentRef.update({...collectUser, id: 1234}));
+      await firebase.assertFails(
+          userDocumentRef.update({...collectUser, name: 1234}));
+      await firebase.assertFails(
+          userDocumentRef.update({...collectUser, email: 1234}));
+      await firebase.assertFails(
+          userDocumentRef.update({...collectUser, icon_url: 1234}));
+      await firebase.assertFails(
+          userDocumentRef.update({...collectUser, role: 1234}));
+      await firebase.assertFails(
+          userDocumentRef.update({...collectUser, level: 1234}));
+    });
   });
 });
