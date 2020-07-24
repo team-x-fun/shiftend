bb9c19c8a4f2ae0627db3aee2d6a08c7599bfd98
kawakita <kawakita@mediba.jp>
2020-07-25 01:43:02 +0900

    ダイアログ部分をファイル分割

--- lib/pages/member/member_item.dart
+++ lib/pages/member/member_item.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shiftend/models/models.dart';
-import 'package:flutter/services.dart';
+import 'package:shiftend/pages/member/member_level_dialog.dart';
 import 'member_provider.dart';
 
 class MemberItem extends StatelessWidget {
@@ -12,7 +12,6 @@ class MemberItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final userLevel = TextEditingController();
     return ChangeNotifierProvider<MemberProvider>(
       create: (_) => MemberProvider(),
       child: Column(
@@ -28,51 +27,7 @@ class MemberItem extends StatelessWidget {
                   context: context,
                   barrierDismissible: true,
                   builder: (BuildContext context) {
-                    return AlertDialog(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      title: Text('${user.name}さんのレベル'),
-                      content: Row(
-                        children: <Widget>[
-                          Expanded(
-                            child: TextField(
-                              autofocus: true,
-                              controller: userLevel,
-                              decoration: const InputDecoration(
-                                labelText: 'レベルの入力',
-                              ),
-                              inputFormatters: <TextInputFormatter>[
-                                WhitelistingTextInputFormatter.digitsOnly,
-                              ],
-                              keyboardType: TextInputType.number,
-                              maxLength: 10,
-                              maxLengthEnforced: true,
-                              maxLines: 1,
-                            ),
-                          ),
-                          const Text(
-                            'Level',
-                            style: TextStyle(fontSize: 18),
-                          ),
-                        ],
-                      ),
-                      actions: <Widget>[
-                        FlatButton(
-                            child: const Text('戻る'),
-                            onPressed: () => {
-                                  print('「戻る」ボタン押した'),
-                                  Navigator.of(context).pop(),
-                                }),
-                        FlatButton(
-                            child: const Text('追加'),
-                            onPressed: () => {
-                                  print('「追加」ボタン押した'),
-                                  model.changeLevel(userLevel.text),
-                                  Navigator.of(context).pop(),
-                                }),
-                      ],
-                    );
+                    return MemberLevelDialog(user, model);
                   },
                 );
               },
