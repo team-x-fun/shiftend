abeedda9ac27455730a1688afc5498b9f482b2c4
Takumi Fujimoto <mottypublic@gmail.com>
2020-09-21 10:29:52 +0900

    feat: シフト追加用の画面追加

--- lib/pages/calendar/widgets/calendar_list_widget.dart
+++ lib/pages/calendar/widgets/calendar_list_widget.dart
@@ -4,6 +4,9 @@ import 'package:provider/provider.dart';
 import 'package:shiftend/models/models.dart';
 import 'package:shiftend/pages/calendar/calendar_state.dart';
 import 'package:shiftend/pages/calendar/widgets/calendar_list_item_widget.dart';
+import 'package:shiftend/pages/calendar_addshift/calendar_addshift_page.dart';
+import 'package:shiftend/repositories/interfaces/user_repository_interface.dart';
+import 'package:shiftend/repositories/mocks/user_repository_mock.dart';
 import 'package:shiftend/util/logger.dart';
 
 class CalendarListWidget extends StatelessWidget {
@@ -24,8 +27,24 @@ class CalendarListWidget extends StatelessWidget {
         },
       );
     }
-    return ListView(
-      children: listItems,
+    listItems.add(
+      ListTile(
+        leading: const Icon(Icons.add),
+        title: const Text('アルバイトを追加する'),
+        onTap: () => {
+          Navigator.of(context).push(
+            MaterialPageRoute<CalenderAddShiftPage>(
+              builder: (context) {
+                return CalenderAddShiftPage();
+              }
+            )
+          )
+        },
+      ),
     );
+    return ListView(
+        children: listItems,
+    );
+    }
   }
-}
+
